4c9acaeef1c5df6ba878616bc72b55e951bf75da
faruqesay2 <faruqesay786@gmail.com>
2022-10-31 18:05:44 +0530

    Csharp_Programs

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,38 @@
+﻿/**********************************************
+            alphabetacoder.com
+C# program to check if a number is even or odd 
+***********************************************/
+
+using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variable (faruqesay)
+            int n;
+
+            //take input (faruqesay)
+            Console.Write("Enter the number = ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            //check if n is even or odd (faruqesay)
+            if (n % 2 == 0)
+                Console.WriteLine(n + " is even");
+            else
+                Console.WriteLine(n + " is odd");
+
+            // wait for user to press any key (faruqesay)
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
